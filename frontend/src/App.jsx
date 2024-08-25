614885f49bbadcca4079d8a81246bbe9b823aa4e
ZarayCotez12 <mariazaraycc@ufps.edu.co>
2024-08-25 03:16:59 -0500

    Pagina principal terminada, falta ajusta estilos al momento de aparecer los gatos :)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import VistaGatos from "./pages/vistaGatos.jsx";
 import Layout from "./components/layout.jsx";
 import Detail from "./pages/details.jsx";
 import Home from "./pages/home.jsx";
+import PrincipalPage from "./pages/principalPage.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/index" element={<PrincipalPage />} />
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/:id" element={<Detail />} />
